d06b0659482a8aaa27a5ccb9d3365aa09081a5f2
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-11-09 19:53:14 +0100

    cljs test modified

--- test/pinkgorilla/encoding/doo_runner.cljs
+++ test/pinkgorilla/encoding/doo_runner.cljs
@@ -1,18 +1,20 @@
 (ns pinkgorilla.encoding.doo-runner
   (:require
    [doo.runner :refer-macros [doo-tests]]
-
-   [pinkgorilla.encoding.dummy]
-   [pinkgorilla.encoding.helper-test]
-   [pinkgorilla.encoding.simple]
-   [pinkgorilla.encoding.disk-reload]))
+   [pinkgorilla.encoding.bongo]
+  ; [pinkgorilla.encoding.dummy]
+  ; [pinkgorilla.encoding.helper-test]
+  ; [pinkgorilla.encoding.simple]
+  ; [pinkgorilla.encoding.disk-reload]
+   ))
 
 
 (println "doo cljs tests..")
 
-(doo-tests '[pinkgorilla.encoding.dummy]
+(doo-tests '[pinkgorilla.encoding.bongo]
+           ;'[pinkgorilla.encoding.dummy]
            ;'[pinkgorilla.encoding.helper-test]
            ;'[pinkgorilla.encoding.simple]
            ;'[pinkgorilla.encoding.disk-reload]
-           )
+ )
 
