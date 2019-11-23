42032473f11679c2230b7e873aae32a339bbf3f3
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-11-23 12:34:25 -0600

    gist loading with empty token

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/encoding "0.0.13"
+(defproject org.pinkgorilla/encoding "0.0.14"
   :description "The encoding of PinkGorilla Notebook."
   :url "https://github.com/pink-gorilla/notebook-encoding"
   :license {:name "MIT"}
