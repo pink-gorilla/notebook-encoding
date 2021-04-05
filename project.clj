f801259047a3604e77f825bb9a772ffad2773240
CI/CD <ci@pinkgorilla.org>
2021-04-05 08:25:24 +0000

    Begin 0.1.19-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/notebook-encoding "0.1.18"
+(defproject org.pinkgorilla/notebook-encoding "0.1.19-SNAPSHOT"
   :description "Encoding of Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/notebook-encoding"
   :license {:name "MIT"}
