3f174cc77ac4004c638fcd1f5acecc0ac1ceffc1
CI/CD <ci@pinkgorilla.org>
2021-04-22 03:14:34 +0000

    Release 0.1.26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/notebook-encoding "0.1.26-SNAPSHOT"
+(defproject org.pinkgorilla/notebook-encoding "0.1.26"
   :description "Encoding of Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/notebook-encoding"
   :license {:name "MIT"}
